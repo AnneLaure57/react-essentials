e67af27b119fc7af8383edb28f8dc048d405cf1b
Anne-Laure CHARLES <anne-laure-charles@orange.fr>
2024-01-12 16:45:38 +0100

    UPDATE : add state and hooks.

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,8 @@
-export default function TabButton({ children }) {
-    return (
-      <li>
-        <button>{children}</button>
-      </li>
-    );
-  }
+export default function TabButton({ children, onSelect }) {
+  console.log('TABBUTTON COMPONENT EXECUTING');
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
